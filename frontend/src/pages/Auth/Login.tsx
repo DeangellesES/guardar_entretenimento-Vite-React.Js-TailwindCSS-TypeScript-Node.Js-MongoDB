0ebb7ac5abd2dc3e2dcc9a0c4e747d03921062e6
Felipe <FelipeDeangelles@hotmail.com>
2026-04-09 17:28:28 -0300

    github

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -3,6 +3,8 @@ import type { FormEvent } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import toast from 'react-hot-toast';
+import { FaEye } from "react-icons/fa";
+import { LuEyeClosed } from "react-icons/lu";
 
 function Login() {
   const { login } = useAuth();
@@ -11,6 +13,8 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [showPassword, setShowPassword] = useState(false)
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
@@ -30,7 +34,7 @@ function Login() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-950 text-white">
-      <div className="w-full max-w-md bg-slate-900/80 border border-slate-800 rounded-xl p-8 shadow-xl">
+      <div className="w-full max-w-md bg-slate-900/80 border border-slate-800 rounded-xl p-8 shadow-xl mx-10">
         <h1 className="text-2xl font-bold mb-6 text-center">Entrar</h1>
 
         <form onSubmit={handleSubmit} className="space-y-4">
@@ -52,15 +56,25 @@ function Login() {
             <label className="block text-sm font-medium mb-1" htmlFor="password">
               Senha
             </label>
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full rounded-md bg-slate-900 border border-slate-700 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              required
-              minLength={6}
-            />
+            <div className='relative'>
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full rounded-md bg-slate-900 border border-slate-700 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                required
+                minLength={6}
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-2 flex items-center text-gray-400 hover:text-gray-200"
+              >
+                {showPassword ? <LuEyeClosed /> : <FaEye />}
+              </button>
+            </div>
+
           </div>
 
           <button
--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -3,6 +3,8 @@ import type { FormEvent } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import toast from 'react-hot-toast';
+import { FaEye } from "react-icons/fa";
+import { LuEyeClosed } from "react-icons/lu";
 
 function Register() {
   const { register } = useAuth();
@@ -11,6 +13,8 @@ function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [showPassword, setShowPassword] = useState(false)
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
@@ -30,7 +34,7 @@ function Register() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-950 text-white">
-      <div className="w-full max-w-md bg-slate-900/80 border border-slate-800 rounded-xl p-8 shadow-xl">
+      <div className="w-full max-w-md bg-slate-900/80 border border-slate-800 rounded-xl p-8 shadow-xl mx-10">
         <h1 className="text-2xl font-bold mb-6 text-center">Criar conta</h1>
 
         <form onSubmit={handleSubmit} className="space-y-4">
@@ -43,7 +47,7 @@ function Register() {
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              pattern="^[^\s@]+@[^\s@]+\.[a-zA-Z]{3,}$"  
+              pattern="^[^\s@]+@[^\s@]+\.[a-zA-Z]{3}$"
               title="Digite um email válido, exemplo: email@email.com"
               className="w-full rounded-md bg-slate-900 border border-slate-700 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
               required
@@ -54,15 +58,25 @@ function Register() {
             <label className="block text-sm font-medium mb-1" htmlFor="password">
               Senha
             </label>
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full rounded-md bg-slate-900 border border-slate-700 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              required
-              minLength={6}
-            />
+            <div className='relative'>
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full rounded-md bg-slate-900 border border-slate-700 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                required
+                minLength={6}
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-2 flex items-center text-gray-400 hover:text-gray-200"
+              >
+                {showPassword ? <LuEyeClosed /> : <FaEye />}
+              </button>
+            </div>
+
           </div>
 
           <button
